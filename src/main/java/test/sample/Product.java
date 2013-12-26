6e7aaa8bfb1e04eb958565d3d42befe7c45f4f3f
Jon Austen <djangofan@gmail.com>
2013-12-26 14:59:40 -0800

    Initial commit.

---
+++ src/main/java/test/sample/Product.java
@@ -0,0 +1,26 @@
+package test.sample;
+ 
+// http://www.mkyong.com/webservices/jax-rs/restfull-java-client-with-java-net-url/
+	
+public class Product {
+ 
+	String name;
+	int qty;
+ 
+	public String getName() {
+		return name;
+	}
+ 
+	public void setName(String name) {
+		this.name = name;
+	}
+ 
+	public int getQty() {
+		return qty;
+	}
+ 
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+ 
+}
---
+++ src/test/java/test/sample/TestGet.java
@@ -0,0 +1,12 @@
+package test.sample;
+
+import org.testng.annotations.Test;
+
+public class TestGet {
+	
+  @Test
+  public void testGetRequest() {
+	  test.sample.client.NetClientGet.getRequest();
+  }
+  
+}
